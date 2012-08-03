c2851447f387e5f3e9a48bf3e98bf71d4db597cd
Tom <tom@shift8creative.com>
2012-08-03 12:24:05 -0700

    directory structure. update to setup script

--- _build/setup.sh
+++ _build/setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 echo ""
+echo "Setting application directory permissions..."
+chmod 777 resources -R
+chmod 775 config/bootstrap/libraries -R
+echo ""
+
+echo ""
 echo "Getting all necessary submodules..."
-git submodule update --init
+git submodule update --init --recursive
 echo ""
 
 echo ""
