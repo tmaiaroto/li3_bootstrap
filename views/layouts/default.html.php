fff3a4336c602a79fcf969cb713853f58b0b87d5
Tom Maiaroto <tom@shift8creative.com>
2012-08-08 12:46:53 -0700

    adding the default Lithium layout and styles

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -1,41 +1,31 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2012, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+?>
 <!doctype html>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://ogp.me/ns/fb#">
+<html>
 <head>
 	<?php echo $this->html->charset();?>
-	<?php $title = $this->title() ? $this->title():''; ?>
-	<title><?=$title ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<?php 
-		echo $this->html->style(array('bootstrap.min.css', 'bootstrap-responsive.min.css', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/smoothness/jquery-ui.css', 'jquery/tipsy.css', 'fluid', 'main'), array('inline' => false));	
-	?>
-	<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script>!window.jQuery && document.write('<script src="/js/jquery/jquery-1.7.2.min.js"><\/script>')</script>	
-	<?php
-		echo $this->html->script(array('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js', 'jquery/jquery.tipsy.js', 'tiny_mce/jquery.tinymce.js', 'bootstrap.min.js'), array('inline' => false));
-	?>
-	<?php
-		echo $this->scripts();
-		echo $this->styles();
-	?>
-	<?php // because there is a navbar. this adds spacing ?>
-	<style type="text/css">
-		body {
-			padding-top: 60px;
-			padding-bottom: 40px;
-		}
-	</style>
+	<title>Application &gt; <?php echo $this->title(); ?></title>
+	<?php echo $this->html->style(array('debug', 'lithium')); ?>
+	<?php echo $this->scripts(); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
-<body>
-	<?=$this->_render('element', 'navbar', array('user' => $this->request()->user)); ?>
-	<div class="container">
-		<?php echo $this->content(); ?>
-		<?=$this->_render('element', 'footer'); ?>
-	</div><!--/.container-->
+<body class="app">
+	<div id="container">
+		<div id="header">
+			<h1>Application</h1>
+			<h2>
+				Powered by <?php echo $this->html->link('Lithium', 'http://lithify.me/'); ?>.
+			</h2>
+		</div>
+		<div id="content">
+			<?php echo $this->content(); ?>
+		</div>
+	</div>
 </body>
 </html>
