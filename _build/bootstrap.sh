fa825f519912be690aaf1af6a88a8b004de7129a
Tom Maiaroto <tom@shift8creative.com>
2012-08-09 00:45:40 -0700

    updates...including a remote installation script

---
+++ _build/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ""
+echo "Welcome to Lithium Bootstrap!"
+echo "----------------------------------"
+echo ""
+echo "Starting the installation process, hold on to your butts..."
+echo ""
+
+git clone git://github.com/tmaiaroto/li3_bootstrap.git .
+clear;
+_build/setup.sh;
